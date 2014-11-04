29cf55f2813f99e600df193894b1d74769681d02
Manuel Nuno Melo <manuel.nuno.melo@gmail.com>
2014-11-04 17:54:14 +0100

    Further work.

--- function_reg.py
+++ function_reg.py
@@ -6,6 +6,7 @@
 #
 import collections
 import numpy
+import types
 
 
 # RegisteredFunction Class #############################################
@@ -20,43 +21,60 @@ class RegisteredFunction():
         self.kwargs = fn_kwargs
         self.name = None
         if name is None:
-            self.name = igetattr(fn,"__name__", None)
+            self.name = getattr(fn,"__name__", None)
         self.make_name_unique()
         if rettype != "auto":
             self.rettype = rettype
         else:
-            self.get_rettype()
+            self.nret, self.rettype = get_rettype()
 
     def __call__(self, *args, **kwargs):
         return self.fn(*args, **kwargs)
 
     def make_name_unique(self):
-        name = self.name
         num = 1
-        if fullname is None:
-            name = "Function"
+        if self.name is None:
+            self.name = "Function"
         if self.parent_col is None:
-            self.name = name
             return
         else:
+            fullname = self.name
             while self.parent_col.fn_list.has_key(fullname):
                 num += 1
-                fullname = "%s_%d" % (name, num)
+                fullname = "%s_%d" % (self.name, num)
             self.name = fullname
 
-def get_rettype(self, fn, fn_args, fn_kwargs):
+def get_rettype(fn, fn_args=(), fn_kwargs=dict()):
     ret = fn(*fn_args, **fn_kwargs)
-    if ret is None:
-        return None
-    if type(ret) == numpy.ndarray:
-        rettype = [[]] 
-    rettype = []
+    typ = get_type(ret, go_deep=True)
+    if any([t==numpy.object for t in typ])
+    if is_iter(typ[0]):
+        return len(typ), typ
+    else:
+        return 1, typ
 
+def get_type(obj, go_deep=False):
+    try:
+        # this is the best I could find for an automated numpy-ctypes translation; got it from the source of ctypeslib.as_ctypes
+        ct = numpy.ctypeslib._typecodes[obj.__array_interface__["typestr"]]
+        return [ct, obj.shape] 
+    except AttributeError:
+        if is_iter(obj):
+            if go_deep:
+                typ = [get_type(subobj) for subobj in obj ]
+                if not all_same(typ):
+                    return typ
+        return get_type(numpy.array(obj))
+    except KeyError:
+        return [numpy.object, obj.shape]
 
+def is_iter(obj):
+    """A handy funcion to identify non-string iterables.
+    """
+    return not isinstance(obj, str) and isinstance(obj, collections.Iterable)
 
-
-
-
+def all_same(lst):
+    return lst.count(lst[0]) == len(lst)
         
 
 class FunctionList():
